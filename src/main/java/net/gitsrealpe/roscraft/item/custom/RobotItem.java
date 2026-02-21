e840f7c59f52c9b34d9b8fd5964132b85631ef94
GitSRealpe <msn-realpe@hotmail.com>
2026-02-21 12:08:44 +0100

    spawning logic now in superclass

--- src/main/java/net/gitsrealpe/roscraft/item/custom/RobotItem.java
+++ src/main/java/net/gitsrealpe/roscraft/item/custom/RobotItem.java
@@ -1,14 +1,19 @@
 package net.gitsrealpe.roscraft.item.custom;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 import net.gitsrealpe.roscraft.block.entity.ROS2LinkBlockEntity;
 import net.gitsrealpe.roscraft.component.ModDataComponents;
+import net.gitsrealpe.roscraft.entity.custom.RobotEntity;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
@@ -17,12 +22,15 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 public class RobotItem extends Item {
     public EntityType<?> robot_entype;
+    public RobotEntity robot;
 
     public RobotItem(Properties properties) {
         super(properties);
@@ -33,25 +41,56 @@ public class RobotItem extends Item {
         Level level = context.getLevel();
         ItemStack stack = context.getItemInHand();
         BlockEntity be = level.getBlockEntity(context.getClickedPos());
-        // block pos as indirect reference to ros2linkBE
+        // Link the item to ROSLink block pos as indirect reference to ros2linkBE
         if (be instanceof ROS2LinkBlockEntity ros2LinkBE) {
             if (!level.isClientSide()) {
-                level.playSound(null, context.getClickedPos(), SoundEvents.NOTE_BLOCK_HARP.value(), SoundSource.BLOCKS,
-                        1.0F, 1.0F);
+                level.playSound(null, context.getClickedPos(), SoundEvents.NOTE_BLOCK_HARP.value(),
+                        SoundSource.BLOCKS, 1.0F, 1.0F);
                 stack.set(ModDataComponents.COORDINATES, ros2LinkBE.getBlockPos());
             }
             return InteractionResult.SUCCESS;
         }
 
+        return tryPlaceRobot(context);
+    }
+
+    protected InteractionResult tryPlaceRobot(UseOnContext context) {
+        Level level = context.getLevel();
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+        // ... check valid bbox for placememnt
         BlockPlaceContext blockPlaceContext = new BlockPlaceContext(context);
         BlockPos blockPos = blockPlaceContext.getClickedPos();
         Vec3 pos = Vec3.atBottomCenterOf(blockPos);
-        AABB aabb = this.robot_entype.getDimensions().makeBoundingBox(pos.x(), pos.y(), pos.z());
-        if (!level.noCollision(null, aabb) || !level.getEntities(null, aabb).isEmpty())
+        AABB aabb = robot_entype.getDimensions().makeBoundingBox(pos.x(), pos.y(), pos.z());
+        if (!level.noCollision(null, aabb) || !level.getEntities(null, aabb).isEmpty()) {
             return InteractionResult.FAIL;
+        }
+
+        ItemStack stack = context.getItemInHand();
+        // build and create the robot entity
+        Consumer<RobotEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, stack, context.getPlayer());
+        RobotEntity robot = (RobotEntity) robot_entype.create(serverLevel);
+        if (robot == null)
+            return InteractionResult.FAIL;
+        consumer.accept(robot);
+        // set the fixed frame and ownership of the robot
 
         // stack.shrink(1);
         return InteractionResult.sidedSuccess(level.isClientSide);
+        // entity placement after creation
+        float rotation = (float) Mth.floor((Mth.wrapDegrees(context.getRotation()) + 22.5F) / 45.0F) * 45.0F;
+        robot.moveTo(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5, rotation, 0.0F);
+        serverLevel.addFreshEntityWithPassengers(robot);
+
+        // Spawn sound effect
+        SoundType soundType = level.getBlockState(blockPos).getSoundType(level, blockPos, robot);
+        robot.playSound(soundType.getBreakSound(), 0.9f, 1.3f);
+
+        robot.gameEvent(GameEvent.ENTITY_PLACE, context.getPlayer());
+        stack.shrink(1);
+        return InteractionResult.sidedSuccess(level.isClientSide());
     }
 
     @Override
--- src/main/java/net/gitsrealpe/roscraft/item/custom/TurtlebotItem.java
+++ src/main/java/net/gitsrealpe/roscraft/item/custom/TurtlebotItem.java
@@ -1,22 +1,9 @@
 package net.gitsrealpe.roscraft.item.custom;
 
-import java.util.function.Consumer;
-
 import net.gitsrealpe.roscraft.entity.ModEntities;
-import net.gitsrealpe.roscraft.entity.custom.TurtlebotEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.gameevent.GameEvent;
 
 public class TurtlebotItem extends RobotItem {
 
@@ -29,29 +16,6 @@ public class TurtlebotItem extends RobotItem {
     public InteractionResult useOn(UseOnContext context) {
         super.useOn(context);
         Level level = context.getLevel();
-        BlockPlaceContext blockPlaceContext = new BlockPlaceContext(context);
-        BlockPos blockPos = blockPlaceContext.getClickedPos();
-        ItemStack itemStack = context.getItemInHand();
-
-        if (level instanceof ServerLevel serverLevel) {
-            Consumer<TurtlebotEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, itemStack,
-                    context.getPlayer());
-            TurtlebotEntity turtlebot = ModEntities.TURTLEBOT.get()
-                    .create(serverLevel, consumer, blockPos, MobSpawnType.SPAWN_EGG, true, true);
-            if (turtlebot == null) {
-                return InteractionResult.FAIL;
-            }
-
-            // place oriented
-            float f = (float) Mth.floor((Mth.wrapDegrees(context.getRotation() - 180.0F) + 22.5F) / 45.0F) * 45.0F;
-            turtlebot.moveTo(turtlebot.getX(), turtlebot.getY(), turtlebot.getZ(), f, 0.0F);
-            serverLevel.addFreshEntityWithPassengers(turtlebot);
-            // serverLevel.addFreshEntity(turtlebot);
-            turtlebot.playSound(SoundEvents.SMITHING_TABLE_USE, 0.9f, 1.3f);
-            turtlebot.gameEvent(GameEvent.ENTITY_PLACE, context.getPlayer());
-        }
-
-        itemStack.shrink(1);
         return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
